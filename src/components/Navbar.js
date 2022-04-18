0bda5fa738d12010b9cfd616cfc190c7154900db
Anik-the-dev <u1304047@student.cuet.ac.bd>
2022-04-18 21:16:35 +0600

    navbar complete

---
+++ src/components/Navbar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { signOut } from 'firebase/auth';
+import auth from '../firebase.init';
+
+const Navbar = () => {
+    const [user] = useAuthState(auth);
+
+    const handleSignOut = () => {
+        signOut(auth);
+
+    }
+    return (
+        <div class="navbar">
+
+            <div class="container flex">
+                <h1 class="logo">sporphy</h1>
+                <ul>
+                    <li><Link to='/'>Home</Link></li>
+                    <li><Link to='/blog'>Blog</Link></li>
+                    <li><Link to='/about'>About</Link></li>
+                    {
+                        user?.uid ?
+                            <li onClick={handleSignOut}>Sign Out</li>
+                            :
+                            <ul>
+                                <li><Link to='/login'>Login</Link></li>
+                                <li><Link to='/signup'>Signup</Link></li>
+                            </ul>
+                    }
+
+
+
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
