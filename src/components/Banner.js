4649f024e2e5b795abedaf8b3a0164bb3b049460
Anik-the-dev <u1304047@student.cuet.ac.bd>
2022-04-18 21:34:07 +0600

    banner done

---
+++ src/components/Banner.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div className='banner'>
+            <div className='container'>
+                <h1>Best Sports Photographer in London.</h1>
+                <p>Sports photography is the capture of sports, games, exercises, and other activities through photography.Sport photographers work for wire services, newspapers, and magazines. Staff photographers to freelancers are among the positions available</p>
+                <button className='btn'>About Me</button>
+                <button className='btn btn-outline'>My Services</button>
+
+            </div>
+
+
+        </div>
+    );
+};
+
+export default Banner;
