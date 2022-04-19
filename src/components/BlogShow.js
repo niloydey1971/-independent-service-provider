3906208105da18a6141853d94c5312ac3ce1f4f5
Anik-the-dev <u1304047@student.cuet.ac.bd>
2022-04-19 10:00:14 +0600

    extra section css done

--- src/components/BlogShow.js
+++ src/components/BlogShow.js
@@ -4,14 +4,14 @@ import race2 from '../../src/race2.png'
 
 const BlogShow = () => {
     return (
-        <div className='container'>
-            <div className='blogshow flex'>
+        <div className='blogshow '>
+            <div className='container grid-two-col'>
                 <img src={race2} alt='photography'></img>
                 <div className='blogshow-text'>
                     <h1>My Best Experiences</h1>
                     <p>Every successful photo has three things in common, and they’re not particularly surprising. The proper aperture, exposure, and focusing distance? The right camera, lens, and tripod? Successful use of hyperfocal distance, ISO invariance, and ETTR? No! The three variables that matter the most in photography are simple: light, subject, and composition. </p>
                     <button className='btn'>
-                        <Link className='btn-text' to='/checkout'>Book Me Now</Link>
+                        <Link className='btn-text' to='/blog'>More Blogs</Link>
                     </button>
                 </div>
 
