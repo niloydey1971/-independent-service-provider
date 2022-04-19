033c5be36c9dcb0084483046d28e15fe39661b64
Anik-the-dev <u1304047@student.cuet.ac.bd>
2022-04-19 09:31:18 +0600

    extra section html done

---
+++ src/components/BlogShow.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import race2 from '../../src/race2.png'
+
+const BlogShow = () => {
+    return (
+        <div className='container'>
+            <div className='blogshow flex'>
+                <img src={race2} alt='photography'></img>
+                <div className='blogshow-text'>
+                    <h1>My Best Experiences</h1>
+                    <p>Every successful photo has three things in common, and they’re not particularly surprising. The proper aperture, exposure, and focusing distance? The right camera, lens, and tripod? Successful use of hyperfocal distance, ISO invariance, and ETTR? No! The three variables that matter the most in photography are simple: light, subject, and composition. </p>
+                    <button className='btn'>
+                        <Link className='btn-text' to='/checkout'>Book Me Now</Link>
+                    </button>
+                </div>
+
+            </div>
+
+        </div>
+    );
+};
+
+export default BlogShow;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Banner from './Banner';
+import BlogShow from './BlogShow';
 import Services from './Services';
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
         <div>
             <Banner></Banner>
             <Services></Services>
+            <BlogShow></BlogShow>
         </div>
     );
 };
